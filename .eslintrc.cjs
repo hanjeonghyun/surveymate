8b762d0e6787a8bbdee9d62337022d183edba165
ssuuu20 <ssuuu20@icloud.com>
2024-01-15 23:42:18 +0900

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'eslint-config-prettier',
+    'plugin:prettier/recommended',
+    'prettier/flowtype',
+    'prettier/react',
+    'prettier/standard'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import PoppinsBoldWoff from '../assets/fonts/PoppinsBold.woff'
+import PoppinsSemiBoldWoff from '../assets/fonts/PoppinsSemiBold.woff'
+import PoppinsMediumWoff from '../assets/fonts/PoppinsMedium.woff'
+
+const GlobalStyle = createGlobalStyle`
+    ${reset};
+    @font-face{
+        font-family: "PoppinsBold";
+        src: url(${PoppinsBoldWoff}) format("woff");
+    }
+    @font-face{
+        font-family: "PoppinsSemiBold";
+        src: url(${PoppinsSemiBoldWoff}) format("woff");
+    }
+    @font-face{
+        font-family: "PoppinsMedium";
+        src: url(${PoppinsMediumWoff}) format("woff");
+    }
+    
+    body{
+        margin: 0vw 5vw;
+        font-family: PoppinsBold, sans-serif; 
+    }
+`;
+
+export default GlobalStyle;
