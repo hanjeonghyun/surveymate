3db0d85d96a9f4ee86ebc4317d5d64f82d4a6b27
leejieun <mariewldms@naver.com>
2024-01-20 23:56:38 +0900

    surveyview마무리

--- src/pages/2. Survey/SurveyView.jsx
+++ src/pages/2. Survey/SurveyView.jsx
@@ -1,9 +1,27 @@
 import React from "react";
-import { TitleWrapper, Title } from "../../components/SurveyComponents";
+import {
+  TitleWrapper,
+  Title,
+  NextButton,
+  ButtonText,
+} from "../../components/SurveyComponents";
 import profile from "../../assets/images/Group 34.svg";
 import styled from "styled-components";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function SurveyView() {
+  const [sameLoggedIn, setSameLoggedIn] = useState(true);
+  const [nickName, setNickName] = useState("강이");
+  //서버로부터 현재 접속자명 불러오기
+  //서버로부터 글 작성자명 불러오기
+  useEffect(() => {
+    if (nickName === "강이") {
+      setSameLoggedIn(true);
+    } else {
+      setSameLoggedIn(false);
+    }
+  }, []);
   return (
     <div>
       <TitleWrapper>
@@ -14,13 +32,16 @@ export default function SurveyView() {
           <img src={profile}></img>
         </div>
         <ProfileWrite>
-          <div>닉네임</div>
+          <div>{nickName}</div>
           <div>0000-00-00</div>
         </ProfileWrite>
+        <ReportButton className={sameLoggedIn ? "none" : ""}>
+          <ReportFont>신고</ReportFont>
+        </ReportButton>
       </Profile>
       <Hr></Hr>
       <Content>
-        <div>제목을 입력하세요(최대 몇자인지)</div>
+        <TitleFont>제목을 입력하세요(최대 몇자인지)</TitleFont>
         <br></br>
         <div>내용을 입력하세요(최대 몇자인지)</div>
         <br></br>
@@ -29,6 +50,24 @@ export default function SurveyView() {
         <div>3. 추가적인 경품이 있다면 기재해 주세요</div>
         <div>4. 누구를 대상으로 진행하는 설문인가요?</div>
       </Content>
+      <Check>
+        <NextButton className={sameLoggedIn ? "none" : ""}>
+          <ButtonText>설문 참여하기</ButtonText>
+        </NextButton>
+        <SameLogged className={sameLoggedIn ? "" : "none"}>
+          <StyledNextButton className="delete">
+            <ButtonText>삭제</ButtonText>
+          </StyledNextButton>
+          <StyledNextButton>
+            <ButtonText>수정 완료</ButtonText>
+          </StyledNextButton>
+        </SameLogged>
+        <AlertWrapper className={sameLoggedIn ? "" : "none"}>
+          <Alert>
+            <p>설문이 등록되었습니다.</p>
+          </Alert>
+        </AlertWrapper>
+      </Check>
     </div>
   );
 }
@@ -41,14 +80,17 @@ const Profile = styled.div`
 `;
 
 const ProfileWrite = styled.div`
-  margin-left: 5vw;
+  margin-left: 10px;
   :first-child {
     margin-bottom: 0.5vh;
     margin-right: 8vw;
   }
-
+  :nth-child(2) {
+    font-size: 12px;
+    font-weight: 400;
+  }
   color: #000;
-  text-align: center;
+  text-align: left;
   font-family: Poppins;
   font-size: 14px;
   font-style: normal;
@@ -63,10 +105,91 @@ const Hr = styled.div`
 const Content = styled.div`
   margin-top: 3vh;
   color: #000;
-
   font-family: Poppins;
   font-size: 14px;
   font-style: normal;
   font-weight: 500;
   line-height: 150%; /* 21px */
+`;
+
+const TitleFont = styled.p`
+  color: #000;
+  font-family: Poppins;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 22px; /* 137.5% */
+`;
+
+const Check = styled.div`
+  .none {
+    display: none;
+  }
+  margin-top: 35vh;
+`;
+const SameLogged = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledNextButton = styled(NextButton)`
+  box-shadow: none;
+  width: 40vw;
+  margin-left: 3vw;
+  margin-right: 3vw;
+  &.delete {
+    background-color: #d9d9d9;
+  }
+`;
+const AlertWrapper = styled.div`
+  position: relative;
+  top: -10px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Alert = styled.div`
+  width: 280px;
+  height: 30px;
+  flex-shrink: 0;
+  border-radius: 999px;
+  background: rgba(65, 65, 65, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  p {
+    color: var(--white, #fff);
+    text-align: center;
+    font-family: Poppins;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+`;
+
+const ReportButton = styled.div`
+  &.none {
+    display: none;
+  }
+  margin-left: 40vw;
+  width: 59px;
+  height: 40px;
+  flex-shrink: 0;
+  border-radius: 10px;
+  background: #d9d9d9;
+  text-align: center;
+`;
+const ReportFont = styled.p`
+  color: #000;
+
+  text-align: center;
+  font-family: Inter;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 40px; /* 307.692% */
+  letter-spacing: -0.408px;
 `;
