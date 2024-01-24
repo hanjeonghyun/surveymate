4bd3512eeab0d885cfe9ca960358dab986d994e0
ssuuu20 <ssuuu20@icloud.com>
2024-01-24 20:37:08 +0900

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import SurveyContent from "./pages/2.Survey/SurveyContent";
+import Main from "./pages/2.Survey/Main";
 import SideBar from "./components/SideBar";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<SurveyContent />} />
+        <Route path="/" element={<Main />} />
       </Routes>
       <SideBar />
     </>
