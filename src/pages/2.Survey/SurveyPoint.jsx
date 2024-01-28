0aa5164bd7c51d29a794aa643df83419e8f5c389
Jeonghyun <limithyunii@gmail.com>
2024-01-29 02:32:19 +0900

    Fix: 바텀시트 컴포넌트 추가

--- src/pages/2.Survey/SurveyPoint.jsx
+++ src/pages/2.Survey/SurveyPoint.jsx
@@ -1,5 +1,6 @@
-import {styled,css} from "styled-components";
+import { styled, css } from "styled-components";
 import * as C from "../../components/SurveyComponents";
+import * as B from "../../components/BottomSheet";
 import Warning from "../../assets/images/dwarning.svg";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
@@ -32,16 +33,20 @@ export default function SurveyPoint() {
   return (
     <>
       <C.TitleWrapper>
-                <BackBtn onClick={handleBack}></BackBtn>
-                <C.Title>설문조사 등록</C.Title>
-                <ThisNextSmallButton disabled={point===0} onClick={clickNext}>
-                  <C.SmallButtonText>다음</C.SmallButtonText></ThisNextSmallButton>
+        <BackBtn onClick={handleBack}></BackBtn>
+        <C.Title>설문조사 등록</C.Title>
+        <ThisNextSmallButton
+          disabled={point === 0}
+          onClick={clickNext}
+        >
+          <C.SmallButtonText>다음</C.SmallButtonText>
+        </ThisNextSmallButton>
       </C.TitleWrapper>
       <ProcessTitle>기간별 사용 포인트 선택</ProcessTitle>
-      <ProcessExplain>
+      <B.ProcessExplain>
         기간별 최대 시간이 되면, 게시물이 자동 삭제됩니다.
         <br /> 1일 = 24시간 기준
-      </ProcessExplain>
+      </B.ProcessExplain>
       <ButtonWrapper>
         {[1, 2, 3, 4, 5, 6, 7].map((day) => (
           <DayButton
@@ -61,12 +66,12 @@ export default function SurveyPoint() {
         )}
         <SizedBox />
       </ButtonWrapper>
-      <InputLabel>사용포인트</InputLabel>
+      <B.InputLabel>사용포인트</B.InputLabel>
       <NotifyBox>
         <DayButtonText>{point} POINT</DayButtonText>
       </NotifyBox>
       <SizedBox />
-      <InputLabel>설문 대상 기간</InputLabel>
+      <B.InputLabel>설문 대상 기간</B.InputLabel>
       <NotifyBox>
         <TermText>0000년 00월 00일 ~ 0000년 00월 00일</TermText>
       </NotifyBox>
@@ -78,118 +83,51 @@ export default function SurveyPoint() {
 function PointBottom({ onCancel, point, handleSubmit }) {
   return (
     <>
-      <BackgroundBottomSheet>
-        <BottomSheetWrapper>
-          <BottomSheetInfo>
-            <InputLabel>설문등록 확인</InputLabel>
-            <ProcessExplain>
+      <B.BackgroundBottomSheet>
+        <B.BottomSheetWrapper>
+          <B.BottomSheetInfo>
+            <B.InputLabel>설문등록 확인</B.InputLabel>
+            <B.ProcessExplain>
               {point}포인트를 사용하여 <br />
               설문을 등록하시겠습니까?
-            </ProcessExplain>
+            </B.ProcessExplain>
             <img
               src={Warning}
               alt='warning'
             />
-          </BottomSheetInfo>
-          <BottomButtonWrapper>
-            <CancelButton onClick={onCancel}>
+          </B.BottomSheetInfo>
+          <B.BottomButtonWrapper>
+            <B.CancelButton onClick={onCancel}>
               <C.ButtonText>취소</C.ButtonText>
-            </CancelButton>
-            <ConfirmButton>
+            </B.CancelButton>
+            <B.ConfirmButton>
               <C.ButtonText onClick={handleSubmit}>등록</C.ButtonText>
-            </ConfirmButton>
-          </BottomButtonWrapper>
-        </BottomSheetWrapper>
-      </BackgroundBottomSheet>
+            </B.ConfirmButton>
+          </B.BottomButtonWrapper>
+        </B.BottomSheetWrapper>
+      </B.BackgroundBottomSheet>
     </>
   );
 }
 
 const BackBtn = styled.button`
-    background: url('src/assets/images/dicon_back.svg') no-repeat;
-    width: 24px;
-    height: 24px;
-    border: none;
-    position: absolute;
-    left: 5vw;
-`
-const ThisNextSmallButton=styled(C.NextSmallButton)`
-    position:absolute;
-    right:5vw;
-    cursor: pointer;
-    ${props=>
-    props.disabled&&
+  background: url("src/assets/images/dicon_back.svg") no-repeat;
+  width: 24px;
+  height: 24px;
+  border: none;
+  position: absolute;
+  left: 5vw;
+`;
+const ThisNextSmallButton = styled(C.NextSmallButton)`
+  position: absolute;
+  right: 5vw;
+  cursor: pointer;
+  ${(props) =>
+    props.disabled &&
     css`
-        background-color:#D9D9D9;
-        pointer-events: none; 
+      background-color: #d9d9d9;
+      pointer-events: none;
     `}
-`;
-
-
-//바텀시트 스타일 컴포넌트
-
-const BackgroundBottomSheet = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.3);
-  display: flex;
-  align-items: flex-end;
-  z-index: 1;
-`;
-
-const BottomSheetWrapper = styled.div`
-  background-color: #ffffff;
-  width: 100vw;
-  margin-left: calc(-50vw + 50%);
-  border: none;
-  border-radius: 20px 20px 0px 0px;
-  align-items: center;
-`;
-
-const BottomSheetInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 5%;
-  text-align: center;
-`;
-
-const BottomButtonWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  padding-bottom: 3vh;
-`;
-
-const CancelButton = styled.button`
-  border-radius: 10px;
-  border: 2px solid #cfc8ff;
-  background: var(--white, #fff);
-  box-shadow: 0px 2px 11px 0px rgba(0, 0, 0, 0.2);
-  color: rgba(207, 200, 255, 1);
-  width: 45vw;
-  height: 5vh;
-  justify-content: center;
-  align-items: center;
-  position: flex;
-  cursor: pointer;
-`;
-
-const ConfirmButton = styled.button`
-  border-radius: 10px;
-  background: #6046ff;
-  color: var(--white, #fff);
-  width: 45vw;
-  height: 5vh;
-  justify-content: center;
-  align-items: center;
-  border: none;
-  box-shadow: 0px 2px 11px 0px rgba(0, 0, 0, 0.2);
-  cursor: pointer;
 `;
 
 //설문조사 등록 페이지 스타일 컴포넌트
@@ -250,16 +188,6 @@ const DayButton = styled.button`
     background-color: #6046ff;
     color: #efedff;
   }
-`;
-
-const InputLabel = styled.p`
-  color: #000;
-  text-align: left;
-  font-family: Poppins;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 800;
-  line-height: normal;
 `;
 
 const DayButtonText = styled.p`
