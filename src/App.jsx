7f870d1ed84ce42af7ba983856d661845f842bba
ssuuu20 <ssuuu20@icloud.com>
2024-01-29 11:53:41 +0900

    초기세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Login from './pages/1. Home/Login'
+import My from './pages/4. My/My'
 
 function App() {
   return (
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path='/' element={<My />} />
       </Routes>
   );
 }
