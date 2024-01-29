a8497ee8fdebcff0f9e8c19208c8389a893291c4
leejieun <mariewldms@naver.com>
2024-01-29 21:55:21 +0900

    비밀번호 찾기 api추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyles';
+import {RecoilRoot} from "recoil";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <GlobalStyle/>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </BrowserRouter>
 );
--- src/pages/1. Home/PwFind.jsx
+++ src/pages/1. Home/PwFind.jsx
@@ -3,10 +3,19 @@ import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import * as C from '../../components/AuthComponents';
 import { Link } from 'react-router-dom';
+import axios from "axios";
+import { atom, useRecoilState, RecoilEnv } from 'recoil';
+RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
+const tokenState=atom({
+    key:"currentToken",
+    default:"",
+});
 
 export default function PwFind() {
+    const [token, setToken]=useRecoilState(tokenState);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [code, setCode]=useState('');
 
     const [isEmail, setIsEmail] = useState(false);
     const [isPassword, setIsPassword] = useState(false);
@@ -23,6 +32,56 @@ export default function PwFind() {
         }
     };
 
+    const onClickEmail=()=>{
+        if (isEmail){
+            axios.post(`/api/auth/password/certification-request`,{
+                receiver:email,
+            })
+            .then((response)=>{
+                console.log(response);
+                alert("이메일로 인증코드가 전송되었습니다")
+            })
+            .catch((response)=>{
+                console.log(response);
+                if (response.response.status===401){
+                    alert('존재하지 않는 아이디입니다.')}
+                else{
+                    alert('서버 통신 에러')
+                }
+            })
+        }else{
+            alert("이메일 형식을 지켜주세요")
+        }
+    }
+
+    const onChangeCode=(e)=>{
+        setCode(e.target.value);
+    }
+
+    const onClickCode=()=>{
+        axios.post("/api/auth/password/certification",{
+            emailAddress: email,
+            code:code,
+        })
+        .then((response)=>{
+            console.log(response);
+            const getToken=response.data.data.emailValidationToken
+            if (getToken){
+                setToken(getToken)
+                alert("인증되었습니다.")
+                console.log(token)
+            }
+        })
+        .catch((response)=>{
+            if (response.response.status===401){
+                alert("잘못된 인증번호입니다.")
+            }else{
+            alert("서버 통신 에러")
+        }
+        })
+        
+    }
+
     const onChangePassword = (e) => {
         const currentPassword = e.target.value;
         setPassword(currentPassword);
@@ -36,19 +95,30 @@ export default function PwFind() {
     };
 
     const navigate = useNavigate();
-    const onChangeButton = (e) => {
+    const onClickButton = (e) => {
         e.preventDefault();
-
-        const goToMain = () => {navigate('/');};
-        const goToLogin = () => window.location.reload();
-        
-        if (!isEmail || !isPassword ? false : true) {
-            alert('회원가입되었습니다.');
-            goToMain();
-        } else {
+        if (isPassword){
+            axios.patch('/api/auth/password/reset',{
+                passwordResetToken: token,
+                newPassword: password,
+            })
+            .then((response)=>{
+                console.log(response);
+                navigate("/login");
+                
+            })
+            .catch((response)=>{
+                if(response.response.status===401){
+                    alert("다른 비밀번호를 입력해주세요")
+                }else{
+                    alert("서버 통신 오류")
+                }
+            })
+        }else{
             alert('이메일 혹은 비밀번호를 정확히 입력해주세요.');
-            goToLogin();
+            window.location.reload();
         }
+        
     };
 
     
@@ -67,14 +137,15 @@ export default function PwFind() {
                     name="email" 
                     value={email} 
                     onChange={onChangeEmail} />
-                <BtnA type='button' onClick={()=>alert("이메일")}></BtnA>
+                <BtnA type='button' onClick={onClickEmail}></BtnA>
             </Wrapper>
         </Content0>
         <Content>
             <C.InputLabel>인증코드 6자리</C.InputLabel>
             <Wrapper>
-                <AuthInput2 placeholder='000000' type='number' />
-                <BtnA type='button' onClick={()=>alert("인증코드")}></BtnA>
+                <AuthInput2 placeholder='000000' type='number'
+                id="code" name="code" value={code} onChange={onChangeCode}/>
+                <BtnA type='button' onClick={onClickCode}></BtnA>
             </Wrapper>
         </Content>
         <Content>
@@ -87,7 +158,7 @@ export default function PwFind() {
                     name="password"
                     value={password}
                     onChange={onChangePassword} />
-                <BtnE type='button'></BtnE>
+                <BtnE type='button' onClick={onClickButton} ></BtnE>
             </Wrapper>
             <P>대소문자, 숫자, 특수문자(@$!*#?&) 포함 8~15자 이내</P>
         </Content>
@@ -95,7 +166,7 @@ export default function PwFind() {
         <Link to="/login">
             <C.NextButton
                 type="submit"
-                onClick={onChangeButton}
+                onClick={onClickButton}
                 className="button">
                 <C.ButtonText>완료</C.ButtonText>
             </C.NextButton>
