32cdd44e061ffbdce2101cd6a9df47bae33f8976
ssuuu20 <ssuuu20@icloud.com>
2024-02-03 00:48:41 +0900

    mypassword

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SideBar from './components/SideBar';
 import MyList from './pages/4. My/MyList';
 import My from './pages/4. My/My'
 import MyView from './pages/4. My/MyView';
+import MyPassword from './pages/4. My/MyPassword';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/mylist3' element={<MyList />} />
         <Route path='/mylist4' element={<MyList />} />
         <Route path='/myview' element={<MyView/>}/>
+        <Route path='/mypassword' element={<MyPassword/>}/>
       </Routes>
       <SideBar/>
     </>
