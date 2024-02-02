c079cb4be77ffc5f737a52b60cfbe0113ea1f32c
ssuuu20 <ssuuu20@icloud.com>
2024-02-03 00:11:59 +0900

    marketcontent

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Survey from './pages/2.Survey/Survey'
 import Market from './pages/3.Market/Market'
 import SideBar from './components/SideBar';
 import MarketView from './pages/3.Market/MarketView';
+import MarketContent from './pages/3.Market/MarketContent';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/survey' element={<Survey />} />
         <Route path='/marketview1' element={<MarketView />} />
         <Route path='/marketview2' element={<MarketView />} />
+        <Route path='/marketcontent' element={<MarketContent />} />
       </Routes>
       <SideBar/>
     </>
