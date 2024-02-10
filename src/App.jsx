11724dcc0476459d4f9d1a980b20293c79fef6fe
Jeonghyun <limithyunii@gmail.com>
2024-02-11 00:02:36 +0900

    fix: 바텀시트, 프사 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,79 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import SideBar from './components/SideBar';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SideBar from "./components/SideBar";
 
-import MyList from './pages/4. My/MyList';
-import My from './pages/4. My/My'
-import MyView from './pages/4. My/MyView';
-import MyPassword from './pages/4. My/MyPassword';
-import MyPoint from './pages/4. My/MyPoint';
-import MyPage from './pages/4. My/MyPage';
-import MyProfileFix from './pages/4. My/MyProfileFix';
-import MyProfile from './pages/4. My/MyProfile';
-import MyOut from './pages/4. My/MyOut';
+import MyList from "./pages/4. My/MyList";
+import My from "./pages/4. My/My";
+import MyView from "./pages/4. My/MyView";
+import MyPassword from "./pages/4. My/MyPassword";
+import MyPoint from "./pages/4. My/MyPoint";
+import MyPage from "./pages/4. My/MyPage";
+import MyProfileFix from "./pages/4. My/MyProfileFix";
+import MyProfile from "./pages/4. My/MyProfile";
+import MyOut from "./pages/4. My/MyOut";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<My />} />
-        <Route path='/mylist1' element={<MyList />} />
-        <Route path='/mylist2' element={<MyList />} />
-        <Route path='/mylist3' element={<MyList />} />
-        <Route path='/mylist4' element={<MyList />} />
-        <Route path='/myview' element={<MyView/>}/>
-        <Route path='/mypassword' element={<MyPassword/>}/>
-        <Route path='/mypointtotal' element={<MyPoint/>}/>
-        <Route path='/mypointplus' element={<MyPoint/>}/>
-        <Route path='/mypointminus' element={<MyPoint/>}/>
-        <Route path='/mypage' element={<MyPage/>}/>
-        <Route path='/myprofile' element={<MyProfile/>}/>
-        <Route path='/myprofilefix' element={<MyProfileFix/>}/>
-        <Route path='/myout' element={<MyOut/>}/>
+        <Route
+          path='/'
+          element={<My />}
+        />
+        <Route
+          path='/mylist1'
+          element={<MyList />}
+        />
+        <Route
+          path='/mylist2'
+          element={<MyList />}
+        />
+        <Route
+          path='/mylist3'
+          element={<MyList />}
+        />
+        <Route
+          path='/mylist4'
+          element={<MyList />}
+        />
+        <Route
+          path='/myview'
+          element={<MyView />}
+        />
+        <Route
+          path='/mypassword'
+          element={<MyPassword />}
+        />
+        <Route
+          path='/mypointtotal'
+          element={<MyPoint />}
+        />
+        <Route
+          path='/mypointplus'
+          element={<MyPoint />}
+        />
+        <Route
+          path='/mypointminus'
+          element={<MyPoint />}
+        />
+        <Route
+          path='/mypage'
+          element={<MyPage />}
+        />
+        <Route
+          path='/myprofile'
+          element={<MyProfile />}
+        />
+        <Route
+          path='/myprofilefix'
+          element={<MyProfileFix />}
+        />
+        <Route
+          path='/myout'
+          element={<MyOut />}
+        />
       </Routes>
-      <SideBar/>
+      <SideBar />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
-import GlobalStyle from './styles/GlobalStyles';
-import {RecoilRoot} from "recoil";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyles";
+import { RecoilRoot } from "recoil";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <GlobalStyle/>
+    <GlobalStyle />
     <RecoilRoot>
       <App />
     </RecoilRoot>
-    
-</BrowserRouter>
+  </BrowserRouter>
 );
