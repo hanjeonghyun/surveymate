6285f4af9e9ce9d5385d830e9b0d3f53c51bcdda
leejieun <mariewldms@naver.com>
2024-02-12 21:07:26 +0900

    mylist,myview api

--- src/pages/4. My/MyList.jsx
+++ src/pages/4. My/MyList.jsx
@@ -4,46 +4,61 @@ import back from "../../assets/images/bicon_back.svg";
 import styled from 'styled-components';
 import { useState,useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
 export default function MyList() {
   const currentPathname=window.location.pathname;
   const [pageTitle,setPageTitle]=useState("응답한 설문조사")
+  const [surveyDummys,setSurveyDummys]=useState([{surveyId:0,title:"데이터 없음",
+  description:"데이터가 존재하지 않습니다.", createdAt: ""}]);
   const navigate = useNavigate();
   const myViewClick=(e)=>{
     navigate('/myview',{
-        state:{
-          pageTitle:pageTitle,
-          title:e.title,
-          content:e.content,
-        }
-      })
+      state:{
+        pageTitle:pageTitle,
+        surveyId:e.surveyId,
+      }
+    }) 
     }
+  const fetchData = async ()=>{
+      //pathname에 따라 받아오는 api 다르게
+      try{
+      if (currentPathname==="/mylist1") {
+        setPageTitle("내가 응답한 설문조사");
+        //survey/respondent
+        const response= await axios.get('/api/survey/respondent');
+        console.log(response.data)
+        {response.data.surveys && setSurveyDummys(response.data.surveys)}
+      } 
+      else if (currentPathname==="/mylist2") {
+        setPageTitle("내가 판매 등록한 설문데이터");
+        const response= await axios.get('/api/data/list/seller');
+        console.log(response.data)
+        {response.data.surveys && setSurveyDummys(response.data.surveys)}
+      } 
+      else if (currentPathname==="/mylist3") {
+        setPageTitle("내가 구매한 설문데이터");
+        const response= await axios.get('/api/data/list/buyer');
+        console.log(response.data)
+        {response.data.surveys && setSurveyDummys(response.data.surveys)}
+      } 
+      else if (currentPathname==="/mylist4") {
+        setPageTitle("내가 등록한 설문조사");
+        const response= await axios.get('/api/survey/registrant');
+        console.log(response)
+        {response.data.surveys && setSurveyDummys(response.data.surveys)};
+      } 
+    }
+    catch(error){
+      console.log("에러 발생", error)
+    }
+  };
    
-  const surveyDummys=
-    [{title: "설문조사 제목1", time: "1일전", content:"설문조사 미리보기가 들어갈 부분입니다. 설문조사 내용을 입력하세요", id:1,
-    status:""}
-    ,{title: "설문조사 제목2", time: "2일전", content:"내용", id:2}
-    ,{title: "설문조사 제목3", time: "2일전", content:"내용", id:3, status:"finished"}
-    ,{title: "설문조사 제목4", time: "2일전", content:"내용", id:4}
-    ,{title: "설문조사 제목5", time: "2일전", content:"내용", id:5}
-    ,{title: "설문조사 제목6", time: "2일전", content:"내용", id:6}
-   ]
   useEffect(() => {
-    //pathname에 따라 받아오는 api 다르게
-    if (currentPathname==="/mylist1") {
-      setPageTitle("내가 응답한 설문조사");
-    } 
-    if (currentPathname==="/mylist2") {
-      setPageTitle("내가 판매 등록한 설문데이터");
-    } 
-    if (currentPathname==="/mylist3") {
-      setPageTitle("내가 구매한 설문데이터");
-    } 
-    if (currentPathname==="/mylist4") {
-      setPageTitle("내가 등록한 설문조사");
-    } 
-
+    fetchData();
+    console.log(surveyDummys)
   }, [currentPathname]);
+
   return (
     <>
     <All>  
@@ -53,15 +68,17 @@ export default function MyList() {
     </TitleWrapper>
     <br></br>
     <ListWrapper>
-      {surveyDummys.map((e)=>{
+      { surveyDummys.map((e)=>{
             return(
-                <EachListWrapper key={e.id} onClick={()=>myViewClick(e)}
-                    className={e.status}>
+                <EachListWrapper key={e.surveyId} onClick={()=>myViewClick(e)}
+                   >
                     <TextTitle>
-                        <Font className="title">{e.title}</Font>
-                        <Font className="time">{e.time}</Font>
+                        <Font className="title">{e.title.length > 15 ? `${e.title.substring(0, 14)}...` : e.title}</Font>
+                        <Font className="time">{e.createdAt}</Font>
                     </TextTitle>
-                        <Font className="content">{e.content}</Font>
+                    <ContentWrapper>
+                      {e.description}
+                    </ContentWrapper>
                 </EachListWrapper>
    
             )
@@ -103,9 +120,27 @@ const EachListWrapper=styled.div`
     background: var(--white, #FFF);
     box-shadow: 0px 2px 11px 0px rgba(0, 0, 0, 0.20);
     margin: 8px 0;
-    padding: 2vh 2vh;
+    padding: 2vh 1.2vh;   
+`
 
-    
+const ContentWrapper=styled.div`
+    width:100%;
+    //331px
+    overflow:hidden;
+    height:auto;
+    text-overflow: ellipsis;
+    word-break: break-all;
+
+    display: -webkit-box;
+   -webkit-line-clamp: 3; // 원하는 라인수
+   -webkit-box-orient: vertical;
+
+    color: #000;
+    font-family: Poppins;
+    font-style: normal;
+    line-height: normal;
+    font-size: 11px;
+    font-weight: 500;
 `
 const Blank=styled.div`
     height:100px;
@@ -121,12 +156,14 @@ const Font=styled.p`
         font-weight: 600;
     }
     &.time{
-        font-size:12px;
+        font-size:11px;
         font-weight: 400;
     }
     &.content{
-        font-size: 12px;
+        font-size: 11px;
         font-weight: 500;
+        white-space: nowrap;
+        text-overflow: ellipsis;
     }
 `
 const TextTitle=styled.div`
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://survey-mate-api.jinhy.uk',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+        ws: true
+      }
+    }
+  }
 })
